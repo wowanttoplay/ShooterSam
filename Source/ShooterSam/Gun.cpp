c1757c43a281c0dc1c941cf6a27745a650646306
fanjie <948420291@qq.com>
2025-09-28 23:40:40 +0800

    feat: implement Gun class with PullTrigger functionality

---
+++ Source/ShooterSam/Gun.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Gun.h"
+
+
+// Sets default values
+AGun::AGun()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	SetRootComponent(Root);
+
+	SkeletalMeshComponent = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("SkeletalMeshComponent"));
+	SkeletalMeshComponent->SetupAttachment(Root);
+}
+
+// Called when the game starts or when spawned
+void AGun::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AGun::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AGun::PullTriger()
+{
+	UE_LOG(LogTemp, Log, TEXT("Bang!") );
+}
+
