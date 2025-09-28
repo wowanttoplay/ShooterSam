1a9229a56885a6bf9d1ef747d9e2f1d35e0f6641
fanjie <948420291@qq.com>
2025-09-28 23:06:02 +0800

    feat: add ShooterBaseWidget class for UI integration

---
+++ Source/ShooterSam/ShooterBaseWidget.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ShooterBaseWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTERSAM_API UShooterBaseWidget : public UUserWidget
+{
+	GENERATED_BODY()
+};
